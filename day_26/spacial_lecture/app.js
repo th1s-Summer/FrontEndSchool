903d39b115a2c69aa709520e580e0b75ceebd2b3
Sebin Kang <93520549+th1s-Summer@users.noreply.github.com>
2021-12-06 22:30:42 +0900

    Create app.js

---
+++ day_26/spacial_lecture/app.js
@@ -0,0 +1,13 @@
+import "./styles.css";
+import { Card, ClassCard, Register, Input } from "./components";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Card />
+      <ClassCard />
+      <Input />
+      <Register />
+    </div>
+  );
+}
